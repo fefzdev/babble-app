ea6fb2850d23132e3afae440756aeb42084ec397
Felix <bouveret.felix@gmail.com>
2021-11-21 12:29:42 +0100

    fix(BabbleErrorPopin): remove ... operator

--- src/components/BabbleErrorPopin/BabbleErrorPopin.js
+++ src/components/BabbleErrorPopin/BabbleErrorPopin.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { TouchableOpacity, Text, View } from 'react-native';
-import colors from '../../assets/style/colors';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeErrorMessage } from '../../store/App';
+import { Text, TouchableOpacity, View } from 'react-native';
 import SvgUri from 'react-native-svg-uri';
+import { useDispatch, useSelector } from 'react-redux';
+
+import colors from '../../assets/style/colors';
+import { removeErrorMessage } from '../../store/App';
 
 export default () => {
   const { errorMessage } = useSelector(state => state.app);
@@ -36,15 +37,15 @@ export default () => {
     return null;
   }
   return (
-    <View style={{ ...styles.popinContainer }}>
+    <View style={styles.popinContainer}>
       <TouchableOpacity
         onPress={() => dispatch(removeErrorMessage())}
-        style={{ ...styles.container }}>
-        <Text style={{ ...styles.text }}> {errorMessage}</Text>
+        style={styles.container}>
+        <Text style={styles.text}> {errorMessage}</Text>
         <SvgUri
           width="24"
           height="24"
-          style={{ ...styles.icon }}
+          style={styles.icon}
           fill={colors.red[1000]}
           source={require('../../assets/icons/Cross.svg')}
         />
