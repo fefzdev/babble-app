e46246bd250651fb549e383b8b82847f982a7d75
Felix <bouveret.felix@gmail.com>
2022-01-14 13:50:00 +0100

    fix(BabbleButton): fix button font size

--- src/components/BabbleButton/BabbleButton.js
+++ src/components/BabbleButton/BabbleButton.js
@@ -14,6 +14,7 @@ export default function BabbleButton({ children, onPress, style }) {
   };
   const textStyle = {
     color: colors.white,
+    fontSize: 16,
   };
 
   const handlePress = () => {
