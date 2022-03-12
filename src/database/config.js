ac6922d1cb946b91b79f029e7991bab023ea6630
Felix <bouveret.felix@gmail.com>
2022-03-12 18:13:29 +0100

    Add env config

--- src/database/config.js
+++ src/database/config.js
@@ -1,21 +1,27 @@
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
+import {
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  DATA_BASE_URL,
+  MESSAGING_SENDER_ID,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+} from '@env';
 import firebase from 'firebase/compat/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBR1jHn6Oq_f8eWFbWPWCT8_7zafO0btsE',
-  authDomain: 'babble-54da8.firebaseapp.com',
-  databaseURL:
-    'https://babble-54da8-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'babble-54da8',
-  storageBucket: 'babble-54da8.appspot.com',
-  messagingSenderId: '880075374588',
-  appId: '1:880075374588:ios:5802bf563c46a33cda3c79',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  databaseURL: DATA_BASE_URL,
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
 
 export { firebase };
