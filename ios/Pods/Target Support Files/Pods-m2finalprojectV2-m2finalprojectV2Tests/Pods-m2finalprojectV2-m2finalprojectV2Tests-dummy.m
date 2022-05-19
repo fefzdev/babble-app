2a09635e6fcabb9f82f7381fc82aec09f2d1fedb
Benoit Lamonica <ben33127@gmail.com>
2022-05-19 16:01:25 +0200

    fix: removed ios folder

--- ios/Pods/Target Support Files/Pods-m2finalprojectV2-m2finalprojectV2Tests/Pods-m2finalprojectV2-m2finalprojectV2Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_m2finalprojectV2_m2finalprojectV2Tests : NSObject
-@end
-@implementation PodsDummy_Pods_m2finalprojectV2_m2finalprojectV2Tests
-@end
