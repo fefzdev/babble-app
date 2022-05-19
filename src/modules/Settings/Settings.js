ce42fd597781b5bfebb8ccd34172d8db3f9f1326
Félix Bouveret <felix@getluko.com>
2022-05-19 14:48:45 +0200

    Fix user type update

--- src/modules/Settings/Settings.js
+++ src/modules/Settings/Settings.js
@@ -6,17 +6,19 @@ import { useSelector } from 'react-redux';
 import BabbleButton from '@/components/BabbleButton/BabbleButton';
 import Background from '@/components/Background';
 import useRepository from '@/database/Model';
+import { useAuthentication } from '@/hooks/useAuthentication';
 
 import RoleModal from '../FirstLogin/components/RoleModal';
 const auth = getAuth();
 function Settings() {
-  const { uid, type } = useSelector(state => state.user);
+  const { type } = useSelector(state => state.user);
+  const { user } = useAuthentication();
   const [modalVisible, setModalVisible] = useState(false);
   const { userRepository } = useRepository();
 
   const onSubmit = userType => {
     setModalVisible(false);
-    userRepository.updateData(uid, {
+    userRepository.updateData(user, {
       type: userType,
     });
   };
