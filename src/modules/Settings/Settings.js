6824b4963b19df1fef6ca739071abf39b5c171ee
Benoit Lamonica <ben33127@gmail.com>
2022-05-19 16:25:10 +0200

    add: settings page

--- src/modules/Settings/Settings.js
+++ src/modules/Settings/Settings.js
@@ -1,35 +1,53 @@
-import { getAuth, signOut } from 'firebase/auth';
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native';
 import { useSelector } from 'react-redux';
 
-import BabbleButton from '@/components/BabbleButton/BabbleButton';
 import Background from '@/components/Background';
+import colors from '@/constants/Colors';
+import fonts from '@/constants/Fonts';
 import useRepository from '@/database/Model';
-import { useAuthentication } from '@/hooks/useAuthentication';
 
 import RoleModal from '../FirstLogin/components/RoleModal';
-const auth = getAuth();
+import SettingsBlock from './components/SettingsBlock';
+import useSettings from './data';
+
 function Settings() {
-  const { type } = useSelector(state => state.user);
-  const { user } = useAuthentication();
+  const settings = useSettings();
+  const { uid } = useSelector(state => state.user);
   const [modalVisible, setModalVisible] = useState(false);
   const { userRepository } = useRepository();
 
-  const onSubmit = userType => {
+  const onSubmit = async userType => {
     setModalVisible(false);
-    userRepository.updateData(user.uid, {
+    await userRepository.updateData(uid, {
       type: userType,
     });
   };
 
+  const handlerFunction = {
+    setModalVisible,
+  };
+
+  useEffect(() => console.log(settings));
+
   return (
     <Background>
-      <Text>{type}</Text>
-      <BabbleButton onPress={() => setModalVisible(true)}>
-        Changer role
-      </BabbleButton>
-      <BabbleButton onPress={() => signOut(auth)}>Disconnect</BabbleButton>
+      {settings.map(({ title, subSettings }) => (
+        <View style={styles.settingContainer} key={title}>
+          <Text style={styles.settingTitle}>{title} :</Text>
+          <View style={styles.subSettingContainer}>
+            {subSettings.map(({ text, subtitle, handle: { fn, prm } }) => (
+              <SettingsBlock
+                text={text}
+                title={subtitle}
+                key={subtitle}
+                onPress={() => handlerFunction[fn](...prm)}
+              />
+            ))}
+          </View>
+        </View>
+      ))}
       <RoleModal
         isVisible={modalVisible}
         onClose={() => setModalVisible(false)}
@@ -38,5 +56,20 @@ function Settings() {
     </Background>
   );
 }
+
+const styles = StyleSheet.create({
+  settingTitle: {
+    ...fonts.xsTitle,
+    marginBottom: 8,
+  },
+  subSettingContainer: {
+    backgroundColor: colors.orange[200],
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  settingContainer: {
+    marginBottom: 20,
+  },
+});
 
 export default Settings;
