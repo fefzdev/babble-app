29035b66bc7a180dfd3d8c736fbf4626289e2ea8
Félix Bouveret <felix@getluko.com>
2022-05-27 00:52:10 +0200

    Add info box for name

--- src/modules/UserAccount/components/NameEditModal.js
+++ src/modules/UserAccount/components/NameEditModal.js
@@ -1,3 +1,4 @@
+import Icon from '@expo/vector-icons/Entypo';
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -5,11 +6,13 @@ import { useSelector } from 'react-redux';
 
 import BabbleInput from '@/components/BabbleInput';
 import BabbleModal from '@/components/BabbleModal';
+import Colors from '@/constants/Colors';
 import Fonts from '@/constants/Fonts';
 import useRepository from '@/database/Model';
+import { UserRoles } from '@/types/UserRoles.enums';
 
 export default function NameEditModal({ isDisplayed, onClose }) {
-  const { name, uid } = useSelector(state => state.user);
+  const { name, uid, type } = useSelector(state => state.user);
   const [nameInput, setNameInput] = useState(name);
   const { userRepository } = useRepository();
 
@@ -17,6 +20,12 @@ export default function NameEditModal({ isDisplayed, onClose }) {
     if (newName) await userRepository.updateData(uid, { name: newName });
     setNameInput(name);
     onClose();
+  };
+
+  const infoMessage = () => {
+    if (type === UserRoles.LISTENER)
+      return 'En tant que Listener, votre nom sera visible par les autres utilisateurs';
+    return 'En tant que Talker, votre nom ne sera pas visible par les autres utilisateurs. Vous restez complètement anonyme.';
   };
 
   return (
@@ -41,6 +50,15 @@ export default function NameEditModal({ isDisplayed, onClose }) {
           placeholder="Votre nom"
           onChangeText={text => setNameInput(text)}
         />
+        <View style={styles.infoBox}>
+          <Icon
+            style={styles.icon}
+            name="info"
+            size={16}
+            color={Colors.orange[1000]}
+          />
+          <Text style={styles.infoBoxText}>{infoMessage()}</Text>
+        </View>
       </View>
     </BabbleModal>
   );
@@ -63,5 +81,20 @@ const styles = StyleSheet.create({
   },
   title: {
     ...Fonts.title,
+  },
+  infoBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: Colors.orange[200],
+    borderRadius: 8,
+    marginTop: 8,
+  },
+  icon: {
+    marginRight: 16,
+  },
+  infoBoxText: {
+    flexShrink: 1,
+    color: Colors.orange[1000],
   },
 });
