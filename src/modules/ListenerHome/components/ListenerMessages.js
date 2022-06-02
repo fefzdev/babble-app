c596bc8f04269721fa61b9d5f8bd2cebd0829e3c
Félix Bouveret <felix@getluko.com>
2022-06-02 23:44:20 +0200

    Fix listener message

--- src/modules/ListenerHome/components/ListenerMessages.js
+++ src/modules/ListenerHome/components/ListenerMessages.js
@@ -1,7 +1,7 @@
 import { format } from 'date-fns';
 import { useEffect } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import UserImage from '@/components/UserImage';
 import useRepository from '@/database/Model';
@@ -9,6 +9,7 @@ import { updateRoom } from '@/store/Rooms';
 
 export default function ListenerMessages({ room, onPress }) {
   const { rooms } = useRepository();
+  const { uid } = useSelector(state => state.user);
   const dispatch = useDispatch();
 
   const fetchRoom = async roomData => {
@@ -21,7 +22,13 @@ export default function ListenerMessages({ room, onPress }) {
         content: 'La room à été créée',
         createdAt: room.timestamp,
       };
-    return room.lastMessage;
+    return {
+      createdAt: room.lastMessage.createdAt,
+      content:
+        room.lastMessage.userUid === uid
+          ? `Vous: ${room.lastMessage.content}`
+          : room.lastMessage.content,
+    };
   };
 
   useEffect(() => {
@@ -36,14 +43,16 @@ export default function ListenerMessages({ room, onPress }) {
         style={styles.thumb}
         image={room.otherUserData.profilePicture}
       />
-      <View style={styles.textBloc}>
-        <Text style={styles.user}>{room.otherUserData.name}</Text>
-        <Text style={styles.message}>{lastMessage().content}</Text>
-      </View>
-      <View>
-        <Text style={styles.time}>
-          {format(new Date(lastMessage().createdAt), 'HH:mm')}
-        </Text>
+      <View style={styles.content}>
+        <View style={styles.textBloc}>
+          <Text style={styles.user}>{room.otherUserData.name}</Text>
+          <Text style={styles.message}>{lastMessage().content}</Text>
+        </View>
+        <View>
+          <Text style={styles.time}>
+            {format(new Date(lastMessage().createdAt), 'HH:mm')}
+          </Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
@@ -54,7 +63,6 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     borderRadius: 50,
-    marginRight: 4,
   },
   user: {
     fontSize: 18,
@@ -69,9 +77,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 8,
   },
-  textBloc: {
-    padding: 4,
+  content: {
+    flexDirection: 'row',
+    marginLeft: 8,
     flexGrow: 1,
+    justifyContent: 'space-between',
+    padding: 4,
   },
   time: {
     fontSize: 12,
